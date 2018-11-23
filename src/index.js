3e96babdcc39ec80a7144dd4de90a204d96570d1
Dibakar Halder <dibakar.haider@partners.rivigo.com>
2018-11-23 16:38:54 +0530

    Initial set up for react app

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+import App from './components/App';
+
+const rootElement = document.getElementById('app');
+
+
+render(<App />, rootElement);
+
+if (module.hot) {
+    module.hot.accept();
+}
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+// const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, "/dist"),
+        filename: "index_bundle.js"
+    },
+    devServer: {
+        inline: true,
+        port: 8080
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader',
+                options: {
+                    presets: ['@babel/preset-react']
+                }
+            }
+        }, {
+            test: /\.css$/,
+            use: [
+                {
+                    loader: 'css-loader'
+                }
+            ]
+        },
+        {
+            test: /\.(png|jpg|gif)$/,
+            use: [
+                {
+                    loader: 'file-loader'
+                }
+            ]
+        }]
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            hash: true,
+            filename: "index.html",  //target html
+            template: "./src/index.html" //source html
+        }),
+    ]
+}
